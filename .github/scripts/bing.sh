215200ebc71e9ed721ebf570490ebf044177b76a
Dawid Ryłko <dawid.rylko@proget.pl>
2023-11-28 19:52:40 +0100

    Add script duration time

--- .github/scripts/bing.sh
+++ .github/scripts/bing.sh
@@ -7,6 +7,15 @@ BLOG_DIR="../../../content/blog/"
 TMP_DIR="$(pwd)/tmp"
 JSON_FILE="bing-submission-api-body.json"
 
+start_time=$(date +%s)
+
+create_temp_directory() {
+  if [ ! -d "$TMP_DIR" ]; then
+    echo "Creating temporary working directory: $TMP_DIR"
+    mkdir "$TMP_DIR"
+  fi
+}
+
 construct_json_body() {
   local json_content="{\"siteUrl\": \"$BASE_URL\", \"urlList\": [\"$BASE_URL/\","
   
@@ -20,7 +29,9 @@ construct_json_body() {
   
   json_content="${json_content%,}]}"
 
+  echo "Constructed JSON Body:"
   echo "$json_content" > "$JSON_FILE"
+  echo
 }
 
 handle_api_key() {
@@ -33,17 +44,23 @@ handle_api_key() {
 submit_to_bing() {
   local bing_api_url="https://ssl.bing.com/webmaster/api.svc/json/SubmitUrlbatch?apikey=${BING_API_KEY}"
 
+  echo "Submitting to Bing API..."
   curl "$bing_api_url" \
     -H "Content-Type: application/json; charset=utf-8" \
     -d "@$JSON_FILE" \
     -s -i
 }
 
-if [ ! -d "$TMP_DIR" ]; then
-  mkdir "$TMP_DIR"
-fi
+create_temp_directory
+
 cd "$TMP_DIR" || exit 1
 
 construct_json_body
 handle_api_key
 submit_to_bing
+
+end_time=$(date +%s)
+duration=$((end_time - start_time))
+
+echo "---------------------------------"
+echo "Process completed in $duration seconds."
