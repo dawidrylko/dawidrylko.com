347835439d216427c1f3697257bc998130747d42
Dawid Ryłko <dawid.rylko@proget.pl>
2023-11-28 20:06:22 +0100

    Test milliseconds

--- .github/scripts/bing.sh
+++ .github/scripts/bing.sh
@@ -7,7 +7,7 @@ BLOG_DIR="../../../content/blog/"
 TMP_DIR="$(pwd)/tmp"
 JSON_FILE="bing-submission-api-body.json"
 
-start_time=$(date +%s)
+start_time=$(date +%s.%3N)
 
 create_temp_directory() {
   if [ ! -d "$TMP_DIR" ]; then
@@ -26,11 +26,17 @@ construct_json_body() {
       json_content+="\"$BASE_URL${BLOGS[i]:${#BLOG_DIR}}\","
     done
   fi
+
+  if [ -z "$json_content" ]; then
+    echo "Error: JSON content is empty."
+    exit 1
+  fi
   
   json_content="${json_content%,}]}"
 
   echo "Constructed JSON Body:"
   echo "$json_content" > "$JSON_FILE"
+  cat "$JSON_FILE"
   echo
 }
 
@@ -45,10 +51,16 @@ submit_to_bing() {
   local bing_api_url="https://ssl.bing.com/webmaster/api.svc/json/SubmitUrlbatch?apikey=${BING_API_KEY}"
 
   echo "Submitting to Bing API..."
-  curl "$bing_api_url" \
+  response=$(curl -s -w "%{http_code}" "$bing_api_url" \
     -H "Content-Type: application/json; charset=utf-8" \
-    -d "@$JSON_FILE" \
-    -s -i
+    -d "@$JSON_FILE")
+    
+  http_code=${response: -3}
+  body=${response::-3}
+  
+  echo "HTTP Response Code: $http_code"
+  echo "API Response Body: $body"
+  echo
 }
 
 create_temp_directory
@@ -59,8 +71,7 @@ construct_json_body
 handle_api_key
 submit_to_bing
 
-end_time=$(date +%s)
-duration=$((end_time - start_time))
-
+end_time=$(date +%s.%3N)
+duration=$(echo "($end_time - $start_time) * 1000" | bc)
 echo "---------------------------------"
-echo "Process completed in $duration seconds."
+echo "Process completed in $duration milliseconds."
