35acc817956aca67df9d80f2182d4a67be314258
Dawid Ryłko <dawid.rylko@proget.pl>
2023-11-29 09:26:45 +0100

    Fix easyindex-cli url

--- .github/scripts/google.sh
+++ .github/scripts/google.sh
@@ -6,7 +6,6 @@ BASE_URL="https://dawidrylko.com"
 BLOG_DIR="../../../content/blog/"
 TMP_DIR="$(pwd)/tmp"
 TMP_FILE="google.csv"
-EASYINDEX_CLI_VERSION="1.0.6"
 
 start_time=$(date +%s.%3N)
 
@@ -14,18 +13,6 @@ log_error() {
   echo "Error: $1"
   exit 1
 }
-
-while getopts o: flag; do
-  case "${flag}" in
-    o) OPERATING_SYSTEM=${OPTARG};;
-  esac
-done
-
-if [[ $OPERATING_SYSTEM == "macos" ]]; then
-  EASYINDEX_CLI_OS="darwin_amd64"
-fi
-
-echo "Using operating system: $OPERATING_SYSTEM"
 
 # Create a temporary working directory
 create_tmp_directory() {
@@ -39,10 +26,8 @@ create_tmp_directory() {
 download_easyindex_cli() {
   EASYINDEX_CLI="./easyindex-cli"
   if [ ! -f "$EASYINDEX_CLI" ]; then
-    EASYINDEX_CLI_URL="https://github.com/usk81/easyindex-cli/releases/download/v${EASYINDEX_CLI_VERSION}/easyindex-cli_${EASYINDEX_CLI_VERSION}_${EASYINDEX_CLI_OS}.tar.gz"
-    echo "Using easyindex_cli URL: $EASYINDEX_CLI_URL"
-    echo "Fetching easyindex_cli binary."
-    curl -s -L "$EASYINDEX_CLI_URL" | tar xz || log_error "Failed to download and extract easyindex_cli binary."
+    echo "Fetching easyindex-cli binary..."
+    curl -s -L "https://github.com/usk81/easyindex-cli/releases/download/v1.0.6/easyindex-cli_1.0.6_linux_amd64.tar.gz" | tar xz || log_error "Failed to download and extract easyindex-cli binary."
     echo
   fi
 }
